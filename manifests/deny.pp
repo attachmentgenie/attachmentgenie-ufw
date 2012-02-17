60159e004e96f13fdba84142969a9c6b7dae2bdc
David Huttleston Jr <dhjr@hddesign.com>
2012-02-17 12:02:40 -0600

    Deny rules supported

---
+++ manifests/deny.pp
@@ -0,0 +1,29 @@
+define ufw::deny($proto="tcp", $port="all", $ip="", $from="any") {
+
+  if $::ipaddress_eth0 != undef {
+    $ipadr = $ip ? {
+      "" => $::ipaddress_eth0,
+      default => $ip,
+    }
+  } else {
+    $ipadr = "any"
+  }
+
+  $from_match = $from ? {
+    "any" => "Anywhere",
+    default => "$from/$proto",
+  }
+
+  exec { "ufw-deny-${proto}-from-${from}-to-${ipadr}-port-${port}":
+    command => $port ? {
+      "all" => "ufw deny proto $proto from $from to $ipadr",
+      default => "ufw deny proto $proto from $from to $ipadr port $port",
+    },
+    unless => $port ? {
+      "all" => "ufw status | grep -E \"$ipadr/$proto +DENY +$from_match\"",
+      default => "ufw status | grep -E \"$ipadr $port/$proto +DENY +$from_match\"",
+    },
+    require => Exec["ufw-default-deny"],
+    before => Exec["ufw-enable"],
+  }
+}
