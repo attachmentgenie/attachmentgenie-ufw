641bde772013982f05889f747d38e0436e9f1543
bram vogelaar <bram@attachmentgenie>
2012-06-25 18:27:52 +0200

    fixing puppet-lint problems

--- manifests/allow.pp
+++ manifests/allow.pp
@@ -1,29 +1,29 @@
-define ufw::allow($proto="tcp", $port="all", $ip="", $from="any") {
+define ufw::allow($proto='tcp', $port='all', $ip='', $from='any') {
 
   if $::ipaddress_eth0 != undef {
     $ipadr = $ip ? {
-      "" => $::ipaddress_eth0,
+      ''      => $::ipaddress_eth0,
       default => $ip,
     }
   } else {
-    $ipadr = "any"
+    $ipadr = 'any'
   }
 
   $from_match = $from ? {
-    "any" => "Anywhere",
+    'any'   => 'Anywhere',
     default => "$from/$proto",
   }
 
   exec { "ufw-allow-${proto}-from-${from}-to-${ipadr}-port-${port}":
     command => $port ? {
-      "all" => "ufw allow proto $proto from $from to $ipadr",
+      'all'   => "ufw allow proto $proto from $from to $ipadr",
       default => "ufw allow proto $proto from $from to $ipadr port $port",
     },
-    unless => $port ? {
-      "all" => "ufw status | grep -E \"$ipadr/$proto +ALLOW +$from_match\"",
+    unless  => $port ? {
+      'all'   => "ufw status | grep -E \"$ipadr/$proto +ALLOW +$from_match\"",
       default => "ufw status | grep -E \"$ipadr $port/$proto +ALLOW +$from_match\"",
     },
-    require => Exec["ufw-default-deny"],
-    before => Exec["ufw-enable"],
+    require => Exec['ufw-default-deny'],
+    before  => Exec['ufw-enable'],
   }
 }
