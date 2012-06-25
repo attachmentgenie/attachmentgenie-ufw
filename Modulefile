2c7a531e353d32577d3cb33edcb45133e67322f2
bram vogelaar <bram@attachmentgenie>
2012-06-25 17:45:39 +0200

    converting to actual puppet module

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'bram-puppet-module-ufw'
+version ''
+source ''
+author 'bram'
+license ''
+summary ''
+description ''
+project_page ''
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,17 @@
-class ufw {
-  package { "ufw":
-    ensure => present,
-  }
+# Class: puppet-module-ufw
+#
+# This module manages puppet-module-ufw
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-module-ufw {
 
-  Package["ufw"] -> Exec["ufw-default-deny"] -> Exec["ufw-enable"]
 
-  exec { "ufw-default-deny":
-    command => "ufw default deny",
-    unless => "ufw status verbose | grep \"[D]efault: deny (incoming), allow (outgoing)\"",
-  }
-
-  exec { "ufw-enable":
-    command => "yes | ufw enable",
-    unless => "ufw status | grep \"[S]tatus: active\"",
-  }
-
-  service { "ufw":
-    ensure => running,
-    enable => true,
-    hasstatus => true,
-    subscribe => Package["ufw"],
-  }
 }
