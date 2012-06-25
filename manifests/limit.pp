a9f55b2aab65c6b55e23a064d7f3f6ee717b43d5
bram vogelaar <bram@attachmentgenie>
2012-06-25 18:21:59 +0200

    fixing puppet-lint problems

--- manifests/limit.pp
+++ manifests/limit.pp
@@ -1,7 +1,7 @@
-define ufw::limit($proto="tcp") {
+define ufw::limit($proto='tcp') {
   exec { "ufw limit $name/$proto":
-    unless => "ufw status | grep -E \"^$name/$proto +LIMIT +Anywhere\"",
-    require => Exec["ufw-default-deny"],
-    before => Exec["ufw-enable"],
+    unless  => "ufw status | grep -E \"^$name/$proto +LIMIT +Anywhere\"",
+    require => Exec['ufw-default-deny'],
+    before  => Exec['ufw-enable'],
   }
 }
