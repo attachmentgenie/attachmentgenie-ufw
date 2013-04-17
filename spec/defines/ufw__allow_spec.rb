7c0a2d4e543a2a3b3827fc9fa58678b6aae80358
Philip Potter <philip.g.potter@gmail.com>
2013-04-17 20:18:03 +0200

    add tests for ufw::allow $from param

--- spec/defines/ufw__allow_spec.rb
+++ spec/defines/ufw__allow_spec.rb
@@ -8,4 +8,12 @@ describe 'ufw::allow', :type => :define do
       with_unless('ufw status | grep -E " +ALLOW +Anywhere"')
     }
   end
+
+  context 'when specifying from address' do
+    let(:params) { {:from => '192.0.2.42'} }
+    it { should contain_exec('ufw-allow-tcp-from-192.0.2.42-to-any-port-all').
+      with_command("ufw allow proto tcp from 192.0.2.42 to any").
+      with_unless('ufw status | grep -E " +ALLOW +192.0.2.42/tcp"')
+    }
+  end
 end
