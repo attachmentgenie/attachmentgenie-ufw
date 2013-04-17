3515bbbb72e5aca6ee8ac050918aeddd7181ee9c
Philip Potter <philip.g.potter@gmail.com>
2013-04-17 20:13:05 +0200

    check exec command and unless lines

--- spec/defines/ufw__allow_spec.rb
+++ spec/defines/ufw__allow_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 
 describe 'ufw::allow', :type => :define do
   let(:title) { 'foo' }
-  it { should contain_exec('ufw-allow-tcp-from-any-to-any-port-all') }
+  context 'basic operation' do
+    it { should contain_exec('ufw-allow-tcp-from-any-to-any-port-all').
+      with_command("ufw allow proto tcp from any to any").
+      with_unless('ufw status | grep -E " +ALLOW +Anywhere"')
+    }
+  end
 end
