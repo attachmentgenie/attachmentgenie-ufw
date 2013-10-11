31130c1f3f7671888b83e6378263f9c4faeb278a
Fredrik Thulin <fredrik@thulin.net>
2013-10-11 16:48:06 +0200

    add 'reject' (simple string replace from 'deny')

---
+++ manifests/reject.pp
@@ -0,0 +1,29 @@
+define ufw::reject($proto='tcp', $port='all', $ip='', $from='any') {
+
+  if $::ipaddress_eth0 != undef {
+    $ipadr = $ip ? {
+      ''      => $::ipaddress_eth0,
+      default => $ip,
+    }
+  } else {
+    $ipadr = 'any'
+  }
+
+  $from_match = $from ? {
+    'any'   => 'Anywhere',
+    default => "$from",
+  }
+
+  exec { "ufw-reject-${proto}-from-${from}-to-${ipadr}-port-${port}":
+    command => $port ? {
+      'all'   => "ufw reject proto $proto from $from to $ipadr",
+      default => "ufw reject proto $proto from $from to $ipadr port $port",
+    },
+    unless  => $port ? {
+      'all'   => "ufw status | grep -E \"$ipadr/$proto +REJECT +$from_match\"",
+      default => "ufw status | grep -E \"$ipadr $port/$proto +REJECT +$from_match\"",
+    },
+    require => Exec['ufw-default-deny'],
+    before  => Exec['ufw-enable'],
+  }
+}
