4fb71985b01a356fbedd43e704f6757193ce3984
Fredrik Thulin <fredrik@thulin.net>
2013-10-14 09:22:41 +0200

    Tighten up 'unless' command regexps.
    It would be a serious bug if some rule was not applied because the
    IP address happened to match another rules IP address while not
    being the same.
    
    E.g. 192.168.0.10 would not be filtered if 192.168.0.105 already was.

--- manifests/deny.pp
+++ manifests/deny.pp
@@ -20,8 +20,8 @@ define ufw::deny($proto='tcp', $port='all', $ip='', $from='any') {
       default => "ufw deny proto $proto from $from to $ipadr port $port",
     },
     unless  => $port ? {
-      'all'   => "ufw status | grep -E \"$ipadr/$proto +DENY +$from_match\"",
-      default => "ufw status | grep -Ee \"$ipadr $port/$proto +DENY +$from_match\" -e \"^${port}/${proto} +DENY +${from_match}$\"",
+      'all'   => "ufw status | grep -E \"^${ipadr}/${proto} +DENY +${from_match}$\"",
+      default => "ufw status | grep -Ee \"^${ipadr} ${port}/${proto} +DENY +${from_match}$\" -e \"^${port}/${proto} +DENY +${from_match}$\"",
     },
     require => Exec['ufw-default-deny'],
     before  => Exec['ufw-enable'],
--- manifests/reject.pp
+++ manifests/reject.pp
@@ -20,8 +20,8 @@ define ufw::reject($proto='tcp', $port='all', $ip='', $from='any') {
       default => "ufw reject proto $proto from $from to $ipadr port $port",
     },
     unless  => $port ? {
-      'all'   => "ufw status | grep -E \"$ipadr/$proto +REJECT +$from_match\"",
-      default => "ufw status | grep -Ee \"$ipadr $port/$proto +REJECT +$from_match\" -e \"^${port}/${proto} +REJECT +${from_match}$\"",
+      'all'   => "ufw status | grep -E \"^${ipadr}/${proto} +REJECT +${from_match}$\"",
+      default => "ufw status | grep -Ee \"^${ipadr} ${port}/${proto} +REJECT +${from_match}$\" -e \"^${port}/${proto} +REJECT +${from_match}$\"",
     },
     require => Exec['ufw-default-deny'],
     before  => Exec['ufw-enable'],
