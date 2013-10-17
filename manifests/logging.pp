d3c2ed161bc3d36fdd692e7e33c2495f7237920b
Igor Galić <i.galic@brainsware.org>
2013-10-17 03:59:58 +0200

    fix lint complaints for ufw::logging

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -1,7 +1,11 @@
+# Define ufw::logging
+#
+#   configure ufw's log verbosity
+#
 define ufw::logging($level='low') {
 
   exec { "ufw-logging-${level}":
-    command => "ufw logging $level",
+    command => "ufw logging ${level}",
     require => Exec['ufw-default-deny'],
     before  => Exec['ufw-enable'],
   }
