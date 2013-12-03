4c986e7ac6235f7cd4e7736e57deec159333ba4c
Igor Galić <i.galic@brainsware.org>
2013-12-03 18:13:41 +0100

    make ufw::logging idempotent
    we achieve this by simply checking for the value in /etc/ufw/ufw.conf.
    This commit fixes issue #14

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -6,6 +6,7 @@ define ufw::logging($level='low') {
 
   exec { "ufw-logging-${level}":
     command  => "ufw logging ${level}",
+    unless   => "grep -qE '^LOGLEVEL=${level}$' /etc/ufw/ufw.conf",
     path     => '/usr/sbin:/bin:/usr/bin',
     provider => 'posix',
     require  => Exec['ufw-default-deny'],
