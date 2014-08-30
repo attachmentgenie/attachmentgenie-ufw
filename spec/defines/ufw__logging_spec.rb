8c8d55483e305f070f880fb713136f7bbca43db6
bob walker <bob@randomness.org.uk>
2014-08-30 19:11:37 +0100

    Add tests for ufw::logging

---
+++ spec/defines/ufw__logging_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'ufw::logging', :type => :define do
+  let(:title) { 'foo' }
+  context 'basic operation' do
+    it { should contain_exec('ufw-logging-low').
+      with_command("ufw logging low").
+      with_unless("grep -qE '^LOGLEVEL=low$' /etc/ufw/ufw.conf")
+    }
+  end
+
+  context 'specifying level' do
+    let(:params) { {:level => 'high'} }
+    it { should contain_exec('ufw-logging-high').
+      with_command("ufw logging high").
+      with_unless("grep -qE '^LOGLEVEL=high$' /etc/ufw/ufw.conf")
+    }
+  end
+
+end
