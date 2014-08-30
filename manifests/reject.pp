b59b7e6822b04a72fdfb1794737c117901cf1b80
bob walker <bob@randomness.org.uk>
2014-08-30 17:24:57 +0100

    Fix lint warning for selector inside resource block

--- manifests/reject.pp
+++ manifests/reject.pp
@@ -14,17 +14,21 @@ define ufw::reject($proto='tcp', $port='all', $ip='', $from='any') {
     default => $from,
   }
 
+  $command  = $port ? {
+    'all'   => "ufw reject proto ${proto} from ${from} to ${ipadr}",
+    default => "ufw reject proto ${proto} from ${from} to ${ipadr} port ${port}",
+  }
+
+  $unless   = $port ? {
+    'all'   => "ufw status | grep -qE '^${ipadr}/${proto} +REJECT +${from_match}$'",
+    default => "ufw status | grep -qEe '^${ipadr} ${port}/${proto} +REJECT +${from_match}$' -qe '^${port}/${proto} +REJECT +${from_match}$'",
+  }
+
   exec { "ufw-reject-${proto}-from-${from}-to-${ipadr}-port-${port}":
     path     => '/usr/sbin:/bin:/usr/bin',
     provider => 'posix',
-    command  => $port ? {
-      'all'   => "ufw reject proto ${proto} from ${from} to ${ipadr}",
-      default => "ufw reject proto ${proto} from ${from} to ${ipadr} port ${port}",
-    },
-    unless   => $port ? {
-      'all'   => "ufw status | grep -qE '^${ipadr}/${proto} +REJECT +${from_match}$'",
-      default => "ufw status | grep -qEe '^${ipadr} ${port}/${proto} +REJECT +${from_match}$' -qe '^${port}/${proto} +REJECT +${from_match}$'",
-    },
+    command  => $command,
+    unless   => $unless,
     require  => Exec['ufw-default-deny'],
     before   => Exec['ufw-enable'],
   }
