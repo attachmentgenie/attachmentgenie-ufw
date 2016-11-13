ba2fd00cf1b33f0726ee4db378e849090b5f9db0
Igor Galić <i.galic@brainsware.org>
2016-11-13 01:21:17 +0100

    spec add spect tests (failing)

--- spec/defines/allow_spec.rb
+++ spec/defines/allow_spec.rb
@@ -34,11 +34,27 @@ describe 'ufw::allow', :type => :define do
       }
     end
 
+    context 'from $ip parameter (any protocol)' do
+      let(:params) { {:ip => '192.0.2.68'} }
+      it { should contain_exec('ufw-allow-IN-any-from-any-to-192.0.2.68-port-all').
+                   with_command("ufw allow  proto any from any to 192.0.2.68").
+                   with_unless("ufw status | grep -qE '^192.0.2.68 +ALLOW +Anywhere( +.*)?$'")
+      }
+    end
+
     context 'from $ip parameter (ipv6)' do
       let(:params) { {:ip => '2a00:1450:4009:80c::1001'} }
       it { should contain_exec('ufw-allow-IN-tcp-from-any-to-2a00:1450:4009:80c::1001-port-all').
         with_command("ufw allow  proto tcp from any to 2a00:1450:4009:80c::1001").
         with_unless("ufw status | grep -qE '^2a00:1450:4009:80c::1001/tcp +ALLOW +Anywhere \\(v6\\)( +.*)?$'")
+      }
+    end
+
+    context 'from $ip parameter (ipv6, any protocol)' do
+      let(:params) { {:ip => '2a00:1450:4009:80c::1001'} }
+      it { should contain_exec('ufw-allow-IN-any-from-any-to-2a00:1450:4009:80c::1001-port-all').
+                   with_command("ufw allow  proto any from any to 2a00:1450:4009:80c::1001").
+                   with_unless("ufw status | grep -qE '^2a00:1450:4009:80c::1001 +ALLOW +Anywhere \\(v6\\)( +.*)?$'")
       }
     end
 
@@ -66,6 +82,14 @@ describe 'ufw::allow', :type => :define do
     it { should contain_exec('ufw-allow-IN-tcp-from-any-to-192.168.42.42-port-8080').
       with_command("ufw allow  proto tcp from any to 192.168.42.42 port 8080").
       with_unless("ufw status | grep -qE '^192.168.42.42 8080/tcp +ALLOW +Anywhere( +.*)?$'")
+    }
+  end
+
+  context 'specifying port for any protocol' do
+    let(:params) { {:port => '8080'} }
+    it { should contain_exec('ufw-allow-IN-any-from-any-to-192.168.42.42-port-8080').
+                 with_command("ufw allow  proto any from any to 192.168.42.42 port 8080").
+                 with_unless("ufw status | grep -qE '^192.168.42.42 8080 +ALLOW +Anywhere( +.*)?$'")
     }
   end
 
