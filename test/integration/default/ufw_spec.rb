2de42cc3b020396e3c83db1cced341db9119ce44
Bram Vogelaar <attachmentgenie@inuits.eu>
2018-02-05 01:28:08 +0100

    appease the lint gods

--- test/integration/default/ufw_spec.rb
+++ test/integration/default/ufw_spec.rb
@@ -1,7 +1,9 @@
-describe service('ufw') do
-  it { should be_installed }
-  it { should be_running }
-end
-describe port(22) do
-  it { should be_listening }
+control 'ufw 01' do
+  impact 1.0
+  title 'ufw package is installed'
+  desc 'Ensures that the ufw package is installed on this system'
+  describe service('ufw') do
+    it { is_expected.to be_installed }
+    it { is_expected.to be_running }
+  end
 end
